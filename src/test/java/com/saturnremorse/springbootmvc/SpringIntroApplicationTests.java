8455bdfd2594e5801e5dbad9c26c8ff1e14d4b12
Saturn-98 <ishaan98jaiswal@gmail.com>
2025-02-04 10:11:53 +0530

    initial commit

---
+++ src/test/java/com/saturnremorse/springbootmvc/SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saturnremorse.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
