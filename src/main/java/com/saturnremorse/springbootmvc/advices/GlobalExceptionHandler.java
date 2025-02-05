1f5a46949372018977db6dfdfc10a52719968c78
Saturn-98 <ishaan98jaiswal@gmail.com>
2025-02-05 13:02:04 +0530

    custom api response

--- src/main/java/com/saturnremorse/springbootmvc/advices/GlobalExceptionHandler.java
+++ src/main/java/com/saturnremorse/springbootmvc/advices/GlobalExceptionHandler.java
@@ -14,22 +14,22 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourseNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFound(ResourseNotFoundException exception){
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourseNotFoundException exception){
         ApiError error = new ApiError(HttpStatus.NOT_FOUND, exception.getMessage(),null);
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ApiResponse<>(error), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleInternalServerError(Exception exception){
+    public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception){
         ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(),null);
-        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ApiResponse<>(error), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleBadRequestError(MethodArgumentNotValidException exception){
+    public ResponseEntity<ApiResponse<?>> handleBadRequestError(MethodArgumentNotValidException exception){
         List<String> errors = exception.getBindingResult().getAllErrors().stream().map(error -> error.getDefaultMessage()).toList();
         ApiError error = new ApiError(HttpStatus.BAD_REQUEST, "input validation errors",errors);
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ApiResponse<>(error), HttpStatus.BAD_REQUEST);
     }
 
 }
